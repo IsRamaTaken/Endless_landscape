c02270d3e0543240503e666556f294617158c01b
Vincent <vincenthuang148@gmail.com>
2020-06-08 22:48:56 +0200

    ajout de classe dans le formulaire

--- formulaire.py
+++ formulaire.py
@@ -1,5 +1,21 @@
-from tkinter import Tk, Label,StringVar,Entry,Button
-from functools import partial
+from tkinter import Tk, Label,StringVar,Entry,Button,font
+
+
+
+def quit():
+    global formulaire
+    global Validation
+    Validation.grid(row=0,column=0,rowspan=7,columnspan=2)
+
+class TitrePartie:
+    def __init__(self,texte,position,fenetre):
+        self.texte=texte
+        self.position=position
+        self.fenetre=fenetre
+        self.titre=Label(self.fenetre,text=texte)
+
+    def positionner(self):
+        self.titre.grid(row=self.position,column=0)
 
 
 class FormulaireRemplir:
@@ -15,11 +31,12 @@ class FormulaireRemplir:
         self.rempli=Entry(fenetre, textvariable=text)
 
 
-
     def positionner(self):
         self.descrip.grid(row=self.position,column=0)
         self.rempli.grid(row=self.position,column=1)
 
+    def getVar(self):
+        return self.rempli.get()
 
 
 
@@ -31,12 +48,16 @@ formulaire.title('Endless landscape')
 Titre=Label(formulaire,text='Configuration de la vidéo',bd=10)
 Titre.grid(row=0,column=0,columnspan=2)
 
-FPS=FormulaireRemplir("Nombre d'images par seconde",1,formulaire,30)
-CadreX=FormulaireRemplir("Largeur du cadre en pixels",2,formulaire,600)
-CadreY=FormulaireRemplir("Hauteur du cadre en pixels",3,formulaire,900)
-VitesseCadreX=FormulaireRemplir("Vitesse de déplacement selon x en pixels par seconde",4,formulaire,30)
-VitesseCadreY=FormulaireRemplir("Vitesse de déplacement selon y en pixels par seconde",5,formulaire,30)
+VideoTitre=TitrePartie("Paramètres Vidéo",1,formulaire)
+Video=FormulaireRemplir("Video",2,formulaire,'Manif.mp4')
+FPS=FormulaireRemplir("Nombre d'images par seconde",2,formulaire,30)
+CadreX=FormulaireRemplir("Largeur du cadre en pixels",3,formulaire,600)
+CadreY=FormulaireRemplir("Hauteur du cadre en pixels",4,formulaire,900)
+VitesseCadreX=FormulaireRemplir("Vitesse de déplacement selon x en pixels par seconde",5,formulaire,30)
+VitesseCadreY=FormulaireRemplir("Vitesse de déplacement selon y en pixels par seconde",6,formulaire,30)
 
+VideoTitre.positionner()
+Video.positionner()
 FPS.positionner()
 CadreX.positionner()
 CadreY.positionner()
@@ -44,5 +65,10 @@ VitesseCadreX.positionner()
 VitesseCadreY.positionner()
 
 
+AffichageProb=Label(formulaire,textvariable='')
+AffichageProb.grid(row=7,column=0,columnspan=2)
+Enter= Button(formulaire,text='Enter',command=quit)
+Enter.grid(row=8,column=0,columnspan=2)
+Validation=Label(formulaire,text='Merci')
 
 formulaire.mainloop()
